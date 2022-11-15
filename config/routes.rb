276d9a11072cf4578d9e0486c79bfbef8ad49588
yuken02 <git@github.com:yuken02/practice.git>
2022-11-15 08:44:38 +0000

    [Add]4-27 Bootstrap,ルーティング,アソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+
+  root to: "homes#top"
   devise_for :users
+  resources :books
+  resources :users
+
+  # get 'users/new'
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
