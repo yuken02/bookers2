98596f2491a94f9426760e56b3d2fb2a6b5ed94e
yuken02 <git@github.com:yuken02/practice.git>
2022-11-16 11:07:16 +0000

    [Add]4-27 indexページの右

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,22 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
+    @user = User.find(current_user.id)
   end
 
   def show
+    @books = Book.all
+    @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
