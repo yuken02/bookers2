75bad2956e912e6b0732dbef463d81eb9ce1bb67
yuken02 <git@github.com:yuken02/practice.git>
2022-11-17 12:30:27 +0000

    [Add]4-27 userのeditページとupdate機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,13 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
 
